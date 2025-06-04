62189934e048ad4e252d3d7dae0985b1d0140da8
daemoon <mintchocolatemoondae@gmail.com>
2025-06-04 19:00:21 +0200

    Refactor tests in test_newest_version.py: enhance documentation, add assertions for random data calculations, and skip known problematic seed

--- tests/versioning/test_newest_version.py
+++ tests/versioning/test_newest_version.py
@@ -3,11 +3,17 @@ import pytest
 import requests
 import json
 
+
+# 1/4
+# Test to ensure that the latest version of the live page is 1.4
+
 def test_version_match(page):
-    # check if the latest version of the live page is 1.4
+    
     page.goto("https://randomuser.me/changelog")
     assert "Version 1.4" in page.content(), "Version 1.4 not found in changelog"
 
+
+# 2/4
 # Verify that newly added nationalities are correct
 
 NATIONALITIES = [
@@ -19,8 +25,6 @@ NATIONALITIES = [
 
 @pytest.mark.parametrize("nat, expected_country", NATIONALITIES)
 def test_new_nationalities(request_context, nat, expected_country):
-
-    """Test to ensure that newly added nationalities are correct in the latest version 1.4."""
     
     url = f"https://randomuser.me/api/?nat={nat}"
     data = helper.fetch_json_data(request_context, url)
@@ -35,6 +39,9 @@ def test_new_nationalities(request_context, nat, expected_country):
     assert country != expected_country, f"Expected country other than {expected_country}, got {country}"
     print("Country 1.3:", country)
 
+
+# 3/4
+# Verify added social security numbers for foreign countries
 
 SSNNAMES = [
     ("br", "CPF"),
@@ -69,7 +76,12 @@ def test_foreign_ssns(request_context, nat, expected_ssn_name):
     print("Social Number 1.3:", socialnumber)
 
 
+# 4/4
 # Test date calculations for dob and registered fields
+
+# N.B. The first 4 tests are run with the seed "testing123" to verify that the date calculations are correct for known values
+# and to verify the logic.
+# These tests are skipped during normal runs.
 
 # Version 1.4 introduced a fix for the date calculations, so we need to ensure that the calculations are correct
 @pytest.mark.test123seed
@@ -132,11 +144,22 @@ def test_registered_calculation_known_1_3():
     # calculation was incorrect in version 1.3, so we expect the age to be 22
     assert years != reg["age"], f"Expected age not to be 5, got {reg['age']}"
 
-def test_dob_calculation_random(request_context):
-    """Test to ensure that the date of birth calculation is correct for random data."""
+
+# Actual tests for random data, these will be run multiple times with different random data
+# TODO: Consider generating a number of random users at once to minimize the known seed error and adjust the key/value search logic accordingly
+
+@pytest.mark.parametrize("i", range(10)) # run the test "i" times with different random data
+def test_dob_calculation_random(request_context, i):
+
+    """Test to ensure that the age calculation from birth is correct for random data."""
     
     url = "https://randomuser.me/api"
     data = helper.fetch_json_data(request_context, url)
+
+    seed = helper.find_key_in_dict(data, "seed")
+    if seed == "d3adb33f":
+        pytest.skip("Skipping test for seed 'd3adb33f' as it is known to cause issues.")
+
     print(f"[DEBUG] Fetching: {url}")
     print(json.dumps(data, indent=2))
     dob = helper.find_key_in_dict(data, "dob")
@@ -145,3 +168,24 @@ def test_dob_calculation_random(request_context):
     print(f"Calculated years since registration: {years}")
     print(f"Generated age: {dob['age']}")
     assert years == dob["age"], f"Expected age to be {dob['age']}, got {years}"
+
+@pytest.mark.parametrize("j", range(10)) # run the test "j" times with different random data
+def test_registered_calculation_random(request_context, j):
+
+    """Test to ensure that the years calculation since registration is correct for random data."""
+    
+    url = "https://randomuser.me/api"
+    data = helper.fetch_json_data(request_context, url)
+
+    seed = helper.find_key_in_dict(data, "seed")
+    if seed == "d3adb33f":
+        pytest.skip("Skipping test for seed 'd3adb33f' as it is known to cause issues.")
+
+    print(f"[DEBUG] Fetching: {url}")
+    print(json.dumps(data, indent=2))
+    reg = helper.find_key_in_dict(data, "registered")
+    years = helper.calculate_years(reg)
+    print(f"Random DOB ver 1.4: {reg}", type(reg))
+    print(f"Calculated years since registration: {years}")
+    print(f"Generated age: {reg['age']}")
+    assert years == reg["age"], f"Expected age to be {reg['age']}, got {years}"
